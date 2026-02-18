c04dec7f509a52405b35dd1d62260d5a2cf1108e
fajar indrawan <fajar.indrawan26@gmail.com>
2026-02-18 22:13:18 +0700

    fix: Tabel

--- resources/views/laporan/detail.blade.php
+++ resources/views/laporan/detail.blade.php
@@ -33,116 +33,6 @@
                     </div>
                 </div>
             </div>
-            <div id="collapseOne" class="accordion-collapse collapse page-header-collapse">
-                <div class="accordion-body pb-2">
-                    <div class="row">
-                        <div class="col-xxl-3 col-md-6">
-                            <div class="card stretch stretch-full">
-                                <div class="card-body">
-                                    <a href="javascript:void(0);" class="fw-bold d-block">
-                                        <span class="d-block">Not Started</span>
-                                        <span class="fs-24 fw-bolder d-block">04</span>
-                                    </a>
-                                    <div class="pt-4">
-                                        <div class="d-flex align-items-center justify-content-between">
-                                            <a href="javascript:void(0);" class="fs-12 fw-medium text-muted">
-                                                <span>Invoices Awaiting</span>
-                                                <i class="feather-link-2 fs-10 ms-1"></i>
-                                            </a>
-                                            <div>
-                                                <span class="fs-12 text-muted">$5,569</span>
-                                                <span class="fs-11 text-muted">(56%)</span>
-                                            </div>
-                                        </div>
-                                        <div class="progress mt-2 ht-3">
-                                            <div class="progress-bar bg-primary" role="progressbar" style="width: 56%">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xxl-3 col-md-6">
-                            <div class="card stretch stretch-full">
-                                <div class="card-body">
-                                    <a href="javascript:void(0);" class="fw-bold d-block">
-                                        <span class="d-block">In Progress</span>
-                                        <span class="fs-24 fw-bolder d-block">06</span>
-                                    </a>
-                                    <div class="pt-4">
-                                        <div class="d-flex align-items-center justify-content-between">
-                                            <a href="javascript:void(0);" class="fs-12 fw-medium text-muted">
-                                                <span>Projects In Progress</span>
-                                                <i class="feather-link-2 fs-10 ms-1"></i>
-                                            </a>
-                                            <div>
-                                                <span class="fs-12 text-muted">16 Completed</span>
-                                                <span class="fs-11 text-muted">(78%)</span>
-                                            </div>
-                                        </div>
-                                        <div class="progress mt-2 ht-3">
-                                            <div class="progress-bar bg-success" role="progressbar" style="width: 78%">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xxl-3 col-md-6">
-                            <div class="card stretch stretch-full">
-                                <div class="card-body">
-                                    <a href="javascript:void(0);" class="fw-bold d-block">
-                                        <span class="d-block">Cancelled</span>
-                                        <span class="fs-24 fw-bolder d-block">02</span>
-                                    </a>
-                                    <div class="pt-4">
-                                        <div class="d-flex align-items-center justify-content-between">
-                                            <a href="javascript:void(0);" class="fs-12 fw-medium text-muted">
-                                                <span>Converted Leads</span>
-                                                <i class="feather-link-2 fs-10 ms-1"></i>
-                                            </a>
-                                            <div>
-                                                <span class="fs-12 text-muted">52 Completed</span>
-                                                <span class="fs-11 text-muted">(63%)</span>
-                                            </div>
-                                        </div>
-                                        <div class="progress mt-2 ht-3">
-                                            <div class="progress-bar bg-warning" role="progressbar" style="width: 63%">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xxl-3 col-md-6">
-                            <div class="card stretch stretch-full">
-                                <div class="card-body">
-                                    <a href="javascript:void(0);" class="fw-bold d-block">
-                                        <span class="d-block">Finished</span>
-                                        <span class="fs-24 fw-bolder d-block">25</span>
-                                    </a>
-                                    <div class="pt-4">
-                                        <div class="d-flex align-items-center justify-content-between">
-                                            <a href="javascript:void(0);" class="fs-12 fw-medium text-muted">
-                                                <span>Conversion Rate</span>
-                                                <i class="feather-link-2 fs-10 ms-1"></i>
-                                            </a>
-                                            <div>
-                                                <span class="fs-12 text-muted">$2,254</span>
-                                                <span class="fs-11 text-muted">(46%)</span>
-                                            </div>
-                                        </div>
-                                        <div class="progress mt-2 ht-3">
-                                            <div class="progress-bar bg-danger" role="progressbar" style="width: 46%">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <!-- [ page-header ] end -->
             <!-- [ Main Content ] start -->
             <div class="main-content">
@@ -162,7 +52,7 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @forelse($students->submission as $item)
+                                            @foreach ($students->submission as $item)
                                                 <tr>
                                                     <td>{{ $loop->iteration }}</td>
                                                     <td>{{ $students->name }}</td>
@@ -170,11 +60,7 @@
                                                     <td>{{ $item->assignment->title }}</td>
                                                     <td>{{ $item->score }}</td>
                                                 </tr>
-                                            @empty
-                                                <tr>
-                                                    <td colspan="5">Belum ada tugas dikumpulkan</td>
-                                                </tr>
-                                            @endforelse
+                                            @endforeach
                                         </tbody>
                                     </table>
                                 </div>
